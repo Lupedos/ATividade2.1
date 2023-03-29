3c62ad27e8adb3d2972972c01ab816208237926b
mewzzie <amanda.duarte722@gmail.com>
2023-03-29 17:20:23 -0300

    testeResultado

---
+++ Assets/asas.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asas : MonoBehaviour
+{
+    int num = 10;
+    int resultado = 5;
+    void Start()
+    {
+      num = num + num; 
+      total = num - total;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
